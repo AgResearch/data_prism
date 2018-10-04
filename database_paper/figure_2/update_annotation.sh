e8a1d6777929a8061641158219bcedab81a56e70
afmcc <alanfmcculloch@gmail.com>
2018-10-04 13:49:32 +1300

    added explanatory note about our use of -max_target_seqs 1

--- database_paper/figure_2/update_annotation.sh
+++ database_paper/figure_2/update_annotation.sh
@@ -4,6 +4,11 @@
 
 gunzip -c agbovine.seq.masked.blast.gz | grep -A 1 "hits found" | grep -v "hits found" | grep -v "# BLASTN 2.2.28" | grep -v "# BLAST processed" | grep -v '\-\-' | ./parse_annotation.py
 
-
-
-
+### note ###
+# Re "Misunderstood parameter of NCBI BLAST impacts the correctness of bioinformatics workflows", Nidhi Shah  Michael G Nute  Tandy Warnow  Mihai Pop  
+# and our use of "-max_target_seqs 1" 
+#
+# Here we have documented our use of this parameter to extract from the database a "similar" (-e 1.0-6)  sequence to the query, so as to 
+# provide a more meaningful label in a plot, (name of probable gene),  than the EST name; it is not essential for our purpose here 
+# that the label is based on the best (i.e. lowest evalue) hit in the database; only that it is based on a sequence which is likely to be
+# expressed from the same gene or gene family member. 
--- database_paper/figure_4/annotate_clusters.sh
+++ database_paper/figure_4/annotate_clusters.sh
@@ -11,7 +11,11 @@ tardis.py -hpctype condor blastx -query _condition_fasta_input_cluster_repset_na
 gunzip -c cluster_repset_names.seq.blastx.gz | grep -A 1 "hits found" | grep -v "hits found" | grep -v "# BLASTX 2.2.28" | grep -v "# BLAST processed" | grep -v '\-\-' | ./parse_annotation.py > probe_genes.txt
 
 ./get_cluster_genes.py > cluster_genes.txt
-
-
-
-
+### note ###
+# Re "Misunderstood parameter of NCBI BLAST impacts the correctness of bioinformatics workflows", Nidhi Shah  Michael G Nute  Tandy Warnow  Mihai Pop
+# and our use of "-max_target_seqs 1"
+#
+# Here we have documented our used of this parameter to extract from the database a "similar" (-e 1.0-6)  sequence to the query, so as to
+# provide a more meaningful label in a plot, (name of probable gene),  than the EST name; it is not essential for our purpose here
+# that the label is based on the best (i.e. lowest evalue) hit in the database; only that it is based on a sequence which is likely to be
+# expressed from the same gene or gene family member.
